b7fc290a6625124751401ede161549dc6a104a3e
Victor Laputsky <valnoros@gmail.com>
2016-11-15 16:43:58 +0000

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from blog.models import Users, Items, Authors, Orders, ItemStatusList, OrderStatusList, Comments
+from blog.models import Users, Items, Authors, Orders, Comments
 admin.site.register(Users)
 admin.site.register(Items)
 admin.site.register(Authors)
-admin.site.register(ItemStatusList)
-admin.site.register(OrderStatusList)
 admin.site.register(Orders)
 admin.site.register(Comments)
 # Register your models here.
