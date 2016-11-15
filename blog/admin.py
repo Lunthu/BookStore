9b17e2fdd6afda5b4ce64501ad22efe76e80780e
Victor Laputsky <valnoros@gmail.com>
2016-11-15 18:33:31 +0000

    admin.py edited online with Bitbucket

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from blog.models import Users, Items, Authors, Orders, Comments
-admin.site.register(Users)
-admin.site.register(Items)
-admin.site.register(Authors)
-admin.site.register(Orders)
-admin.site.register(Comments)
+admin.site.register((Users, Items, Authors, Orders, Comments,))
 # Register your models here.
